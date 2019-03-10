2e33c7d83bf16f07356831b41b3cc3902a9327b9
Matthew Mangione <mjmangione1@gmail.com>
2019-03-09 16:35:33 -0800

    Cleaned assignment/changed plotting packages

--- analysis_final.R
+++ analysis_final.R
@@ -1,8 +1,11 @@
+
 library(dplyr)
 library(tidyr)
 library(ggplot2)
 library(usmap)
+library(tools)
 
+setwd("C:/Users/Matt/Documents/INFO2012/final_assignment/")
 crime_rates_county <- read.csv("crime_data_w_population_and_crime_rate.csv", stringsAsFactors = FALSE) 
 rent_price <- read.csv("county_median_rental_price.csv", stringsAsFactors = FALSE)
 
@@ -24,29 +27,45 @@ crime_rates_county <- crime_rates_county %>%
             BURGLRY, LARCENY, MVTHEFT, ARSON, population, FIPS_ST)
 
 # attaches FIPS codes to the rental info
-fips_codes <- readxl::read_xlsx("fips_codes.xlsx") 
-colnames(fips_codes) <- c("summary","state_code", "county_code", "ignore1", "ignore2", "ignore3", "RegionName")
-fips_codes <- fips_codes %>% 
-     mutate(fips = (paste0(state_code, county_code))) %>% 
-     select(RegionName, fips)
-rent_w_fips <- left_join(rent_price, fips_codes, by = "RegionName")
+#fips_codes <- readxl::read_xlsx("fips_codes.xlsx") 
+#colnames(fips_codes) <- c("summary","state_code", "county_code", "ignore1", "ignore2", "ignore3", "RegionName")
+#fips_codes <- fips_codes %>% 
+#     mutate(fips = (paste0(state_code, county_code))) %>% 
+#     select
+
+
+#rent_w_fips <- left_join(rent_price, fips_codes, by = "RegionName")
 
 # attaches crime df to the rent df with the FIPS codes for plotting
-crime_vs_rent <- left_join(rent_w_fips, crime_rates_county, by = "county_name")
-View(crime_vs_rent)
+crime_vs_rent <- left_join(rent_price, crime_rates_county, by = "county_name")
+#View(crime_vs_rent)
 
-#--------------------------QUESTION 1-----------------------------#
+#--------------------------QUESTION 2-----------------------------#
 # What is the most common crime per each category of rent price? (for 2016)
 
-crime_vs_rent <- crime_vs_rent %>% 
-     filter(year ==)
-
+crime_vs_rent_2016 <- crime_vs_rent %>% 
+     filter(year == 2016)
+breaks <- c(0, 1000, 1500, 2000, 2500, Inf)
+crime_vs_rent_2016 <- mutate(crime_vs_rent_2016, rent_category = cut(rent_value, 
+               breaks, labels = c("$0 to $1000", "$1000 to $1500", "$1500 to $2000", "$2000 to $2500", "$2500+")))
+View(crime_vs_rent_2016)
 #-----------------------------------------------------------------#
 
-# for plotting based off counties // NOT COMPLETE // USES OLD VARIABLES 
-View(crime_vs_rent_2016)
-plot_usmap(regions = "counties", data = rent_2016, values = "X2016.08") + 
-     labs(title = "US Counties", subtitle = "This is a blank map of the counties of the United States.") + 
-     theme(panel.background = element_rect(colour = "black", fill = "lightblue"))
+# below is the county map for plotting stuff
+# along with an example for plotting a map.
+county_map <- map_data('county')
+county_map <- county_map %>% 
+     mutate(State = state.abb[match(toTitleCase(county_map$region), state.name)]) %>% 
+     mutate(RegionName = paste(toTitleCase(county_map$subregion),"County"))
+View(county_map)
+crime_vs_rent_map_2016 <- left_join(crime_vs_rent_2016, county_map, by = c("State", "RegionName"))
+View(crime_vs_rent_map_2016)
 
+ggplot(data = crime_vs_rent_map_2016) +
+     geom_polygon(aes(x = long, y = lat, group = group, fill = rent_category)) +
+     scale_fill_manual(values=c("#b2182b", "#ef8a62", "#fddbc7", "#d1e5f0", "#67a9cf", "#2166ac")) +
+     ggtitle("Percent Difference in Forest Levels (1992 - 2016)") +
+     xlab("") +
+     ylab("") +
+     coord_quickmap()
 
